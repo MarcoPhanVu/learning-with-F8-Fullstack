df14cbf4bc6618f0e75e6876c66a31eec1211f0f
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2023-01-12 15:19:15 +0700

    adding textarea for Ace

--- Toast/toastScript.js
+++ Toast/toastScript.js
@@ -1,1 +1,9 @@
-allSmallContainer = document.querySelectorAll(".result");
+let textArea0 = document.querySelector("#textKeepingZone");
+
+function checkForText() {
+    console.log(textArea0);
+} 
+
+textArea0.addEventListener("input", (ev) => {
+    console.log(ev.target.value);
+})
